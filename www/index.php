87a95fbc4073c7c9ddab9333c588781e30b35940
bob.a8v <bobruskin@gmail.com>
2018-04-06 03:38:17 +1200

    Edit echo <br>/n

--- www/index.php
+++ www/index.php
@@ -2,8 +2,8 @@ Database connection details:
 <?php
 #	phpinfo(1|4|8);
 	$conn = pg_connect(getenv("DATABASE_URL"));
-	echo chr(10); // same as /n
-	echo $conn . "\n";
-	echo "user:password@host:port/database\n";
-	echo $_ENV['DATABASE_URL'] . "\n";
+	echo "<br>" . chr(10); // same as /n
+	echo $conn . "<br>\n";
+	echo "user:password@host:port/database" . "<br>\n";
+	echo $_ENV['DATABASE_URL'] . "<br>\n";
 ?>
