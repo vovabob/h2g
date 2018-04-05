8300d44c1cc8936a1f3681694572aa9cd7dd2c73
bob.a8v <bobruskin@gmail.com>
2018-04-06 03:24:18 +1200

    Edited echo output: //n newlines

--- www/index.php
+++ www/index.php
@@ -1,6 +1,8 @@
+Database connection details:
 <?php
 #	phpinfo(1|4|8);
 	$conn = pg_connect(getenv("DATABASE_URL"));
-	echo $conn;
-	echo $_ENV['DATABASE_URL'];
+	echo \n$conn\n;
+	echo "user:password@host:port/database\n";
+	echo $_ENV['DATABASE_URL']\n;
 ?>
