98c31854d20f30eb3c8beda7484c33e3c40ff6d6
bob.a8v <bobruskin@gmail.com>
2018-04-06 03:32:46 +1200

    .

--- www/index.php
+++ www/index.php
@@ -2,7 +2,8 @@ Database connection details:
 <?php
 #	phpinfo(1|4|8);
 	$conn = pg_connect(getenv("DATABASE_URL"));
-	echo \n$conn\n;
+	echo chr(10); // same as /n
+	echo $conn . "\n";
 	echo "user:password@host:port/database\n";
-	echo $_ENV['DATABASE_URL']\n;
+	echo $_ENV['DATABASE_URL'] . "\n";
 ?>
