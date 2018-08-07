2c7cd20e409eab2e841d39d3833721f939a42d59
vovabob <bobruskin@gmail.com>
2018-08-07 23:53:32 +1200

    change to point to db4free.net

--- includes/DatabaseConnection.php
+++ includes/DatabaseConnection.php
@@ -1,3 +1,4 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=ijdb_sample;charset=utf8', 'ijdb_sample', 'mypassword');
+//$pdo = new PDO('mysql:host=localhost;dbname=ijdb_sample;charset=utf8', 'ijdb_sample', 'mypassword');
+$pdo = new PDO('mysql:host=db4free.net;dbname=vovabobdb;port=3306;charset=utf8', 'vovabobuser', 'v1o2v3a4');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
